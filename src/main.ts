a7d6d0c4418f08f3cc2cd13f71e5724a958a926e
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-07 17:16:19 +0200

    Create base for Todolist class

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,30 @@ interface Todo {
     task: string;
     completed: boolean;
     priority: 1 | 2 | 3;
-
 }
+
+class TodoList implements Todo {
+    task: string;
+    completed: boolean;
+    priority: 1 | 2 | 3;
+
+    constructor(task: string, completed: boolean, priority: 1 | 2 | 3) {
+        this.task = task;
+        this.completed = completed;
+        this.priority = priority;
+    }
+
+    // Method addTodo(task: string, priority: number): boolean
+
+    // Method markTodoCompleted(todoIndex: number): void
+
+    // Method getTodos(): Todo[] 
+
+    // Method saveToLocalStorage(): void
+
+    // Method loadFromLocalStorage(): void 
+}
+
+// Separate class for todo localstorage? Check class 2 1:5 min in (utility-klass)
+
+// Create separate files for interface/classes and use import/export? 
