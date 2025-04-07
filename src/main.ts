2d7a0498587fbc6866e9a125556fd327de99933e
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-07 20:44:42 +0200

    Test adding todos and pushing to array

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,15 @@ class TodoList implements Todo {
     // Method loadFromLocalStorage(): void 
 }
 
+const todo1 = new TodoList('Vacuum', false, 1);
+const todo2 = new TodoList('Shop', false, 2);
+
+const todos: TodoList[] = [];
+todos.push(todo1);
+todos.push(todo2);
+
+console.log(todos);
+
 // Separate class for todo localstorage? Check class 2 1:5 min in (utility-klass)
 
 // Create separate files for interface/classes and use import/export? 
