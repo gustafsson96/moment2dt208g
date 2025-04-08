3a6355baf09656985213344895894081a565458d
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-08 20:33:49 +0200

    Update todolist class to use interface correctly for an array and uncomment necessary methods

--- src/main.ts
+++ src/main.ts
@@ -6,36 +6,32 @@ interface Todo {
     priority: 1 | 2 | 3;
 }
 
-class TodoList implements Todo {
-    task: string;
-    completed: boolean;
-    priority: 1 | 2 | 3;
+class TodoList {
+    private todos: Todo[] = [];
 
-    constructor(task: string, completed: boolean, priority: 1 | 2 | 3) {
-        this.task = task;
-        this.completed = completed;
-        this.priority = priority;
+    constructor() {
+        console.log("Todo list created!");
+        // Collect from local storage here? From a separate class? 
     }
 
-    // Method addTodo(task: string, priority: number): boolean
+    addTodo(task: string, priority: 1 | 2 | 3): boolean {
 
-    // Method markTodoCompleted(todoIndex: number): void
+    }
 
-    // Method getTodos(): Todo[] 
+    markTodoCompleted(todoIndex: number): void {
+
+    }
+
+    getTodos(): Todo[] {
+
+    }
 
     // Method saveToLocalStorage(): void
 
     // Method loadFromLocalStorage(): void 
 }
 
-const todo1 = new TodoList('Vacuum', false, 1);
-const todo2 = new TodoList('Shop', false, 2);
 
-const todos: TodoList[] = [];
-todos.push(todo1);
-todos.push(todo2);
-
-console.log(todos);
 
 // Separate class for todo localstorage? Check class 2 1:5 min in (utility-klass)
 
