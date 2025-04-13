1136373c0195f4a31bad2e44b0ff9ee4eae45081
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-13 10:08:42 +0200

    Update comments for todo list class

--- src/classes.ts
+++ src/classes.ts
@@ -1,14 +1,15 @@
 import { Todo } from './interfaces';
 import { loadTodosFromLocalStorage } from './localstorage';
 
+// Class for a list of todos
 export class TodoList {
     private todos: Todo[] = [];
 
     constructor() {
-        console.log("Todo list created!");
         this.todos = loadTodosFromLocalStorage();
     }
 
+    // Add todo after validation
     addTodo(task: string, priority: 1 | 2 | 3): boolean {
         if (!task.trim() || ![1, 2, 3].includes(priority)) {
             return false;
@@ -29,6 +30,7 @@ export class TodoList {
         this.todos.splice(todoIndex, 1); 
     }
 
+    // Toggle status of todo
     markTodoCompleted(todoIndex: number): void {
         const todo = this.todos[todoIndex];
         if (todo) {
@@ -36,14 +38,17 @@ export class TodoList {
         }
     }
 
+    // Return list of todos
     getTodos(): Todo[] {
         return this.todos;
     }
 
+    // Get todos from local storage
     loadFromLocalStorage(data: Todo[]): void {
         this.todos = data;
     }
 
+    // Return todos as JSON string for saving
     saveToLocalStorage(): string {
         return JSON.stringify(this.todos);
     }
